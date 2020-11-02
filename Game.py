22658c819e0140b0f7249f841912b25f4d324a2c
Juliette Hortemel <juliette.hortemel@etu.emse.fr>
2020-11-02 13:36:46 +0100

    Update Game Alive

--- Game.py
+++ Game.py
@@ -6,13 +6,40 @@ from Configuration import *
 
 class Game:
     """Defines the rules."""
-    
-    def __init__(self):
-        pass
-    
+
+
     def alive(self, cell): 
+        if isOfState(cell) == True :
+            compteur = 0
+            for i in range (len(voisin(cell))):
+               if isOfState(voisin(cell[i])) == True :
+                   compteur += 1
+            if compteur == 2 or compteur == 3 :
+                return True
+            else :
+                return False
+        else :
+            compteur2 = 0
+            for i in range (len(voisin(cell))) :
+                if isOfState(voisin(cell[i])) == True :
+                    compteur2 += 1
+            if compteur2 == 3 :
+                return True
+            else :
+                return False
+                    
+        
         return True
 
     def voisin(self, cell):
         return []
 
+class GameOfLife(Game):
+
+    def alive(self, configuration,  cell): 
+        # self.voisin(cell) ->liste de cellules
+        # 
+        return True
+
+    def voisin(self, cell):
+        return []
